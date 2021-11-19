4ad54428ea6ebb72209da15a88f1f0976af5bda1
Max Kydanchuk <maxym.kydanchuk@gmail.com>
2021-11-19 11:23:39 +0200

    done with binary search algorithm

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const data = require('./prize.json');
 const algorithms = require('./binary-search.js');
 
-
 const sort = algorithms.quickSort(data.prizes, 'year', 'asc');
 const result = algorithms.binarySearch(sort, 'year', '1994');
 
